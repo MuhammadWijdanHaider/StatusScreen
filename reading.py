e825ba16d4db63961308ce0690375c2697584dd5
Muhammad Wijdan Haider <ghostwijdan12@gmail.com>
2023-08-21 18:50:25 +0500

    Complete Testing

--- reading.py
+++ reading.py
@@ -3,9 +3,14 @@ data = []
 tags = []
 
 with open("data.json", "r") as json_file:
-    loaded_data = load(json_file)
+    loaded_data:dict = load(json_file)
 
-
+# temporary blok for testing
+END_SUFFIX = loaded_data.get("end_separator")
+START_PREFIX = loaded_data.get("start_separator")
+INLINE_SPE = loaded_data.get("in_separator")
+SETTER_SPE = loaded_data.get("setter")
+# temporary blocks ends
 
 with open(r"D:\Vaults\Hobby Projects\Hobby Project\data.txt", "r") as file:
 
@@ -15,22 +20,20 @@ with open(r"D:\Vaults\Hobby Projects\Hobby Project\data.txt", "r") as file:
 
 def striping_func(data:list):
     temp = {}
-    separator = "["
-    e_separator = "]"
     for i in data:
-        if separator in i and e_separator in i:
-            te = (i.removeprefix("[")).split(":")
+        if START_PREFIX in i and END_SUFFIX in i:
+            te = (i.removeprefix(START_PREFIX)).split(SETTER_SPE)
             temp[te[0]] = te[1]
     for i in temp:
-        print(i)
+        print(f"Value: {temp.get(i)}, extension: {type(temp.get(i))}")
         
 
 
 
-# for i in range(len(data)):
-#     if data[i] == "~Status~":
-#         striping_func(data)
-#     else:
-#         pass
+for i in range(len(data)):
+    if data[i] == "~Status~":
+        striping_func(data)
+    else:
+        pass
 
 
