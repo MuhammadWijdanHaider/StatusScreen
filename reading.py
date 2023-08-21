b66a83a1c1c019182a9c58652d1f826184dbd37d
Muhammad Wijdan Haider <wijdanhaider12@outlook.com>
2023-08-21 18:05:25 +0500

    init

---
+++ reading.py
@@ -0,0 +1,36 @@
+from json import load
+data = []
+tags = []
+
+with open("data.json", "r") as json_file:
+    loaded_data = load(json_file)
+
+print(loaded_data)
+
+
+with open(r"D:\Vaults\Hobby Projects\Hobby Project\data.txt", "r") as file:
+
+    for i in file.readlines():
+        data.append(i.strip())
+        
+
+def striping_func(data:list):
+    temp = {}
+    separator = "["
+    e_separator = "]"
+    for i in data:
+        if separator in i and e_separator in i:
+            te = (i.removeprefix("[")).split(":")
+            temp[te[0]] = te[1]
+    for i in temp:
+        print(i)
+        
+
+
+
+# for i in range(len(data)):
+#     if data[i] == "~Status~":
+#         striping_func(data)
+#     else:
+#         pass
+
---
+++ separator_set.py
@@ -0,0 +1,44 @@
+from json import load, dump
+
+
+
+def text_doc(path:str):
+    with open("data.json", "r") as json_file:
+        data = load(json_file)
+    temp = []
+    with open(rf"{path}", "r") as file:
+        for i in file.readlines():
+            temp = (i.split("$"))
+            data[temp[0].strip()] = temp[1].strip()
+    json_readLoad(data=data)
+
+
+def json_readLoad(file_path:str = "", data:dict = {}):
+    if len(file_path) != 0:
+        with open(rf"{file_path}", "r") as json_file:
+            p_data:dict = load(json_file)
+        with open("data.json", "r") as json_file:
+            data = load(json_file)
+        temp = ["end_separator", "start_separator", "in_separator", "setter"]
+        for i in range(4):
+            data[temp[i]] = p_data.get(temp[i].strip())
+        with open("data.json", "w") as json_file:
+            dump(data, json_file, indent=4)
+    elif len(data) != 0:
+        with open("data.json", "w") as json_file:
+            dump(data, json_file, indent=4)
+    else:
+        'Custom error messgae here later'
+
+
+    
+
+
+
+
+text_doc("D:\Vaults\Hobby Projects\Hobby Project\dummy.txt")
+
+json_readLoad(file_path="temp.json")
+
+
+
